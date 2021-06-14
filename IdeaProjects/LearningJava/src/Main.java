27e00a614717954bd665c9a8dedf9e9e0943a2c5
River Chiang <chchiang2000@hotmail.com>
2021-06-14 23:14:41 +0800

    add LearningJava

--- IdeaProjects/LearningJava/src/Main.java
+++ IdeaProjects/LearningJava/src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press Enter to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time");
+    }
+
+    public static double calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate,
+                                               double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+
+        // System.out.println("Your total meal price is " + result);
+
+        return result;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World from the IntelliJ IDE!");
 
@@ -30,5 +50,79 @@ public class Main {
 
         System.out.println(studentFirstName + " " + studentLastName +
                 "now has a GPA of " + studentGPA);
+
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+
+        int inputtedNum = scanner.nextInt();
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy the good luck a friend brings you");
+        } else {
+            System.out.println("Your shoe selection will make you very happy today");
+        }
+
+        boolean isOnRepeat = true;
+        while(isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off repeat? If so, answer yes");
+            String  userInput = input.next();
+
+            if(userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song");
+
+        String question = "What is the largest planet in our solar system?";
+        String choiceOne = "earth";
+        String choiceTwo = "jupiter";
+        String choiceThree = "saturn";
+
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        System.out.println(question);
+
+        // Write a print statement giving the answer choices
+        System.out.println("Choose one of the following: " +
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
+
+        // Have the user input an answer
+        // Scanner scanner = new Scanner(System.in);
+        // Retrieve the user's input
+        String strInput = scanner.next();
+
+        // If the user's input matches the correctAnswer...
+        // then the user is correct and we want to print out a congrats message to the user.
+        if(correctAnswer.equals(strInput.toLowerCase())) {
+            System.out.println("Congrats! That's the correct answer");
+        } else {
+            System.out.println("You are incorrect. The correct answer is " + correctAnswer);
+        }
+
+        System.out.println("Welcome to your new job");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted!");
+
+        /*
+        calculateTotalMealPrice(15, .2, .08);
+        calculateTotalMealPrice(25, .18, .08);
+        */
+
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
+        System.out.println(groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
+
+        double result = Math.pow(2, 5);
+        System.out.println(result);
     }
 }
