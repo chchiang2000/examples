f53458361bb549eeccd774dcd9ef4976c7d277b9
River Chiang <riverchiang@gmail.com>
2021-06-14 08:33:26 +0800

    modified Using_the_PHP_Library.php for localhost

--- Mobile01/php-examples/MongoDB/Using_the_PHP_Library.php
+++ Mobile01/php-examples/MongoDB/Using_the_PHP_Library.php
@@ -1,10 +1,14 @@
 <?php
 
-$dbhost = 'river:river@gettingstarted-shard-00-00.pvk3r.mongodb.net:27017';
+// $dbhost = 'river:river@gettingstarted-shard-00-00.pvk3r.mongodb.net:27017';
+$dbhost = 'localhost:27017';
 
-$dbname = 'GettingStarted';
+// $dbname = 'GettingStarted';
+$dbname = 'test';
 
 echo (class_exists("MongoDB\Driver\Manager")), "<br>";
+
+echo "test";
 
 $mongoClient = new MongoDB\Driver\Manager('mongodb://' . $dbhost);
 
